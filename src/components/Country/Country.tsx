1275e6fc03e60c0f1e9622ea59f10bab8d887fcd
asm-saim <saim777m@gmail.com>
2026-09-04 22:37:24 +0600

    grid alignment resorted

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -9,9 +9,10 @@ export default function Country({ country }: CountryProps) {
     <div className="country">
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Common Name: {country.name.common}</h3>
-      <h4>Official Name: {country.name.official}</h4>
-      <h4>Capital: {country.capital.capital}</h4>
-      <h4>Population: {country.population.population}</h4>
+      <h5>Official Name: {country.name.official}</h5>
+      <h5>Capital: {country.capital.capital}</h5>
+      <h5>Population: {country.population.population}</h5>
+      <button>Visited</button>
     </div>
   );
 }
