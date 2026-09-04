61b9892c101b8325f94ea67c1f01fb9e9bdd9db6
asm-saim <saim777m@gmail.com>
2026-09-04 23:27:57 +0600

    toggle button with bg color implemented

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -6,21 +6,18 @@ export type CountryProps = {
   country: CountryType;
 };
 export default function Country({ country }: CountryProps) {
-  
-const [visited, setVisited] = useState(false);
+  const [visited, setVisited] = useState(false);
   const handleVisited = () => {
     setVisited(!visited);
-  }
+  };
   return (
-    <div className="country">
+    <div className={`country ${visited ? "country-bg" : ""}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Common Name: {country.name.common}</h3>
       <h5>Official Name: {country.name.official}</h5>
       <h5>Capital: {country.capital.capital}</h5>
       <h5>Population: {country.population.population}</h5>
-      <button onClick={handleVisited}>
-        {visited ? "Visited" : "Mark as Visited"}
-      </button>
+      <button onClick={handleVisited}>{visited ? "Visited" : "Mark as Visited"}</button>
     </div>
   );
 }
