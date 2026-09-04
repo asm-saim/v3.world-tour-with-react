d11dbf5b5c9c5b9537b9dea9c805aa92a952faf4
asm-saim <saim777m@gmail.com>
2026-09-04 23:21:39 +0600

    toggle button implemented

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,10 +1,16 @@
 import type { CountryType } from "../../types";
 import "./Country.css";
+import { useState } from "react";
 
 export type CountryProps = {
   country: CountryType;
 };
 export default function Country({ country }: CountryProps) {
+  
+const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  }
   return (
     <div className="country">
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
@@ -12,7 +18,9 @@ export default function Country({ country }: CountryProps) {
       <h5>Official Name: {country.name.official}</h5>
       <h5>Capital: {country.capital.capital}</h5>
       <h5>Population: {country.population.population}</h5>
-      <button>Visited</button>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Mark as Visited"}
+      </button>
     </div>
   );
 }
