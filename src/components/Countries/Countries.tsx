4b2283a0ea1dc73c3ea475c6c3a0dd045c85dba2
asm-saim <saim777m@gmail.com>
2026-09-05 09:31:37 +0600

    lift up state implemented

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -14,19 +14,33 @@ export default function Countries({ countriesPromise }: CountriesProps) {
   // State to keep track of visited countries
   const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([]);
 
+  // State to keep track of visited flags
+  const [visitedFlags, setVisitedFlags] = useState<string[]>([]);
+
   // Function to handle marking a country as visited
   const handleVisited = (country: CountryType) => {
     const visited = [...visitedCountries, country];
     setVisitedCountries(visited);
   };
 
+  const handleFlags = (flags: string): void => {
+    const markVisitedFlags = [...visitedFlags, flags];
+    setVisitedFlags(markVisitedFlags);
+  };
+
   return (
     <>
       <h2>Countries: {countries.length}</h2>
       <h3>Visited Countries: {visitedCountries.length}</h3>
+      <h3>Visited Flags: {visitedFlags.length}</h3>
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country} handleVisited={handleVisited}></Country>
+          <Country
+            key={country.ccn3.ccn3}
+            country={country}
+            handleVisited={handleVisited}
+            handleFlags={handleFlags}
+          ></Country>
         ))}
       </div>
     </>
--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -5,13 +5,15 @@ import { useState } from "react";
 export type CountryProps = {
   country: CountryType;
   handleVisited: (country: CountryType) => void;
+  handleFlags: (flag: string) => void;
 };
-export default function Country({ country, handleVisited }: CountryProps) {
+export default function Country({ country, handleVisited, handleFlags }: CountryProps) {
   const [visited, setVisited] = useState<boolean>(false);
   const handleVisitedCountry = () => {
     setVisited(!visited);
     handleVisited(country);
   };
+
   return (
     <div className={`country ${visited ? "country-bg" : ""}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
@@ -20,6 +22,7 @@ export default function Country({ country, handleVisited }: CountryProps) {
       <h5>Capital: {country.capital.capital}</h5>
       <h5>Population: {country.population.population}</h5>
       <button onClick={handleVisitedCountry}>{visited ? "Visited" : "Mark as Visited"}</button>
+      <button onClick={() => handleFlags(country.flags.flags.png)}>Mark Flag as Visited</button>
     </div>
   );
 }
